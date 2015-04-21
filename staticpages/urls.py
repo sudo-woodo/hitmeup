0f150150d504ce37fa1e420bd880e31cc6f39d72
Ethan Chan <metakirby5@gmail.com>
2015-04-21 13:39:47 -0700

    jinja and static scaffolding

--- staticpages/urls.py
+++ staticpages/urls.py
@@ -3,5 +3,5 @@ from django.views.generic import TemplateView
 import views
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='staticpages/index.html'), name='index'),
+    url(r'^$', views.index, name='index'),
 ]
--- staticpages/views.py
+++ staticpages/views.py
@@ -1,3 +1,10 @@
+from django.views.generic import TemplateView
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'staticpages/index.html', {
+        'css': [
+            'staticpages/css/index.css'
+        ]
+    })
