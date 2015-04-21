aa0fc42a80efe087cbc92f1d8d94fbd4bd1341b1
Ethan Chan <metakirby5@gmail.com>
2015-04-21 12:44:47 -0700

    add some bootstrap

--- hitmeup/settings.py
+++ hitmeup/settings.py
@@ -33,12 +33,15 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = (
+    # Stock
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Custom
     'hitmeup',
     'staticpages',
 )
