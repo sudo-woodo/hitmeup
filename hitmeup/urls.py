d03a7bc63cc6ac03551b012767cb48f90d4f7caf
Ethan Chan <metakirby5@gmail.com>
2015-04-23 19:18:42 -0700

    i think that's all the docs...

--- hitmeup/urls.py
+++ hitmeup/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     # url(r'^$', 'hitmeup.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('staticpages.urls', namespace='staticpages')),
+    url(r'^admin/', include(admin.site.urls)),
 ]
