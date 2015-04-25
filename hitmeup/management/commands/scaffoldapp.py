dadb30834c857d2ed7a6fd912c7381d72aedfbac
Ethan Chan <metakirby5@gmail.com>
2015-04-24 20:31:45 -0700

    move scaffolding stuff to settings.py & the unit test grind

--- hitmeup/management/commands/scaffoldapp.py
+++ hitmeup/management/commands/scaffoldapp.py
@@ -1,6 +1,8 @@
 from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
 import os
 import errno
+
 
 class Command(BaseCommand):
     help = "Scaffolds an app directory with the typical directories and files you need."
@@ -8,30 +10,18 @@ class Command(BaseCommand):
 
     TABS = "    "
 
-    DIRS = [
-        (os.path.join('{app}', 'static'), "holds project static assets"),
-        (os.path.join('{app}', 'static', '{app}'), "holds {app}'s static assets"),
-        (os.path.join('{app}', 'static', '{app}', 'js'), "holds {app}'s Javascript files"),
-        (os.path.join('{app}', 'static', '{app}', 'css'), "holds {app}'s CSS files"),
-        (os.path.join('{app}', 'static', '{app}', 'img'), "holds {app}'s image files"),
-        (os.path.join('{app}', 'templates'), "holds project templates"),
-        (os.path.join('{app}', 'templates', '{app}'), "holds {app}'s templates"),
-    ]
-
-    FILES = [
-        (os.path.join('{app}', 'urls.py'), "describes {app}'s routes")
-    ]
-
     def add_arguments(self, parser):
         parser.add_argument('app', metavar='directory', help="the directory to scaffold")
 
     def handle(self, *args, **options):
         app = options['app']
 
+        # check if folder exists
         if not os.path.isdir(app):
             raise CommandError("App directory \"%s\" does not exist." % app)
 
-        for path, desc in self.DIRS:
+        # generate dirs
+        for path, desc in settings.SCAFFOLDAPP_DIRS:
             path = path.format(app=app)
             desc = desc.format(app=app)
 
@@ -41,14 +31,22 @@ class Command(BaseCommand):
             except OSError as e:
                 if e.errno == errno.EEXIST and os.path.isdir(path):
                     self.stdout.write(self.TABS + "%s already exists." % path)
-                else:
-                    raise
+                else: raise
 
-        for path, desc in self.FILES:
+        # generate files
+        for path, desc in settings.SCAFFOLDAPP_FILES:
             path = path.format(app=app)
             desc = desc.format(app=app)
 
             self.stdout.write("Making file {path}, which {desc}...".format(path=path, desc=desc))
+            # make containing dir
+            container = os.path.dirname(path)
+            try:
+                os.makedirs(container)
+            except OSError as e:
+                if e.errno == errno.EEXIST and os.path.isdir(container): pass
+                else: raise
+            # make file
             try:
                 open(path).close()
             except IOError:
---
+++ hitmeup/tests.py
@@ -0,0 +1,148 @@
+import shutil
+import tempfile
+from django.test.utils import override_settings
+import os
+from StringIO import StringIO
+from django.core.management import call_command
+from django.core.management.base import CommandError
+from django.test import TestCase
+
+# Create your tests here.
+from hitmeup import settings
+
+
+TEST_SCAFFOLDAPP_DIRS = [
+    (os.path.join('{app}', 'static'), "holds project static assets"),
+    (os.path.join('{app}', 'static', '{app}'), "holds {app}'s static assets"),
+    (os.path.join('{app}', 'static', '{app}', 'js'), "holds {app}'s Javascript files"),
+    (os.path.join('{app}', 'static', '{app}', 'css'), "holds {app}'s CSS files"),
+    (os.path.join('{app}', 'static', '{app}', 'img'), "holds {app}'s image files"),
+    (os.path.join('{app}', 'templates'), "holds project templates"),
+    (os.path.join('{app}', 'templates', '{app}'), "holds {app}'s templates"),
+    (os.path.join('{app}', 'something', '{app}', 'somethingelse'), "test -p functionality"),
+    ]
+
+TEST_SCAFFOLDAPP_FILES = [
+    (os.path.join('{app}', 'urls.py'), "describes {app}'s routes"),
+    (os.path.join('{app}', 'unmade', 'folder', 'urls.py'), "test -p functionality"),
+]
+
+
+@override_settings(SCAFFOLDAPP_DIRS=TEST_SCAFFOLDAPP_DIRS, SCAFFOLDAPP_FILES=TEST_SCAFFOLDAPP_FILES)
+class ScaffoldAppTestCase(TestCase):
+    """
+    Tests usage of `manage.py scaffoldapp`.
+    """
+
+    def setUp(self):
+        # change to temp dir
+        self.__orig_dir = os.path.dirname(os.path.abspath(__file__))
+        self.__temp_dir = tempfile.mkdtemp()
+        os.chdir(self.__temp_dir)
+
+        # setup app dir
+        self.app = 'app'
+        os.mkdir(self.app)
+
+    def tearDown(self):
+        # restore
+        os.chdir(self.__orig_dir)
+        shutil.rmtree(self.__temp_dir)
+        del self.__orig_dir
+        del self.__temp_dir
+
+    def test_normal(self):
+        """
+        Test the normal use case.
+        """
+        # set up mock stdout
+        out = StringIO()
+        call_command('scaffoldapp', self.app, stdout=out)
+
+        # test dir generation
+        for path, desc in settings.SCAFFOLDAPP_DIRS:
+            path = path.format(app=self.app)
+            desc = desc.format(app=self.app)
+
+            self.assertTrue(os.path.isdir(os.path.join(self.__temp_dir, path)),
+                            "Directory \"%s\" does not exist" % path)
+            self.assertIn(desc, out.getvalue(), "Description \"%s\" not in stdout" % desc)
+
+        # test file generation
+        for path, desc in settings.SCAFFOLDAPP_FILES:
+            path = path.format(app=self.app)
+            desc = desc.format(app=self.app)
+
+            self.assertTrue(os.path.isfile(os.path.join(self.__temp_dir, path)),
+                            "File \"%s\" does not exist" % path)
+            self.assertIn(desc, out.getvalue(), "Description \"%s\" not in stdout" % desc)
+
+    def test_requires_arg(self):
+        """
+        Test that scaffoldapp fails with inappropriate args.
+        """
+        # test no args
+        with self.assertRaises(CommandError):
+            call_command('scaffoldapp')
+
+        # test too many args
+        with self.assertRaises(CommandError):
+            call_command('scaffoldapp', 'extraneous', 'args')
+
+    def test_requires_directory(self):
+        """
+        Test that scaffoldapp fails when a directory that doesn't exist is provided.
+        """
+        with self.assertRaises(CommandError):
+            call_command('scaffoldapp', 'DOES_NOT_EXIST')
+
+    def test_partial_directories(self):
+        """
+        Test that the correct messages print for already existing directories
+        """
+        # set up mock stdout
+        out = StringIO()
+
+        # make half the dirs
+        made_dirs = settings.SCAFFOLDAPP_DIRS[:len(settings.SCAFFOLDAPP_DIRS)//2]
+        for path, desc in made_dirs:
+            path = path.format(app=self.app)
+            os.makedirs(path)
+
+        # make half the files
+        made_files = settings.SCAFFOLDAPP_FILES[:len(settings.SCAFFOLDAPP_FILES)//2]
+        for path, desc in made_files:
+            path = path.format(app=self.app)
+            open(path, 'a+').close()
+
+        call_command('scaffoldapp', self.app, stdout=out)
+
+        # assert the correct message shows up in dir generation
+        msg = "already exists"
+        for item in settings.SCAFFOLDAPP_DIRS:
+            path, desc = item
+            path = path.format(app=self.app)
+            desc = desc.format(app=self.app)
+
+            self.assertTrue(os.path.isdir(os.path.join(self.__temp_dir, path)),
+                            "Directory \"%s\" does not exist" % path)
+
+            if item in made_dirs:
+                self.assertIn(desc, out.getvalue(), "Message \"%s\" is not in stdout" % msg)
+            else:
+                self.assertIn(desc, out.getvalue(), "Description \"%s\" not in stdout" % desc)
+
+        # assert the correct message shows up in file generation
+        for item in settings.SCAFFOLDAPP_FILES:
+            path, desc = item
+            path = path.format(app=self.app)
+            desc = desc.format(app=self.app)
+
+            self.assertTrue(os.path.isfile(os.path.join(self.__temp_dir, path)),
+                            "File \"%s\" does not exist" % path)
+
+            if item in made_files:
+                self.assertIn(desc, out.getvalue(), "Message \"%s\" is not in stdout" % msg)
+            else:
+                self.assertIn(desc, out.getvalue(), "Description \"%s\" not in stdout" % desc)
+
