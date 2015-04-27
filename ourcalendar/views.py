b1e3f2c970bc4decdd1fd0dff336c3cb5231206e
RaphaelLu <ronilu66@hotmail.com>
2015-04-26 18:01:11 -0700

    Calendar can load events from database now

--- ourcalendar/views.py
+++ ourcalendar/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def calendar(request):
-    return render(request, 'ourcalendar/index.jinja')
+    context = {'events': Event.objects.all()}
+    return render(request, 'ourcalendar/index.jinja',context)
