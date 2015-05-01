82ccd2114417ea3a276ac67cec23c4d6e2334289
Ethan Chan <metakirby5@gmail.com>
2015-05-01 05:10:08 -0700

    use factories for more robust testing

--- user_accounts/tests.py
+++ user_accounts/tests.py
@@ -1,13 +1,17 @@
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.test import TestCase, Client
+from util import random_string
+import factory
 
 SIGNUP_URL = reverse('user_accounts:signup')
 LOGIN_URL = reverse('user_accounts:login')
 HOME_URL = reverse('static_pages:home')
-LOGIN_INFO = {'username': 'name', 'password': 'password'}
-SIGNUP_INFO = LOGIN_INFO.copy()
-SIGNUP_INFO.update({'email': 'email@website.com'})
+UserFactory = factory.make_factory(User,
+    username=factory.LazyAttribute(lambda _: random_string()),
+    password=factory.PostGenerationMethodCall('set_password', random_string()),
+    email=factory.LazyAttribute(lambda u: '%s@example.com' % u.username),
+)
 
 
 class SignUpTestCase(TestCase):
@@ -16,11 +20,21 @@ class SignUpTestCase(TestCase):
     """
     def setUp(self):
         self.client = Client()
+        self.password = random_string()
+        self.user = UserFactory.create(password=self.password)
+        self.LOGIN_INFO = {
+            'username': self.user.username,
+            'password': self.password,
+        }
+        self.SIGNUP_INFO = self.LOGIN_INFO.copy()
+        self.SIGNUP_INFO.update({
+            'email': self.user.email,
+        })
 
     def test_standard(self):
         # Tests a standard registering of a new user
-        response = self.client.post(SIGNUP_URL, SIGNUP_INFO)
-        user = User.objects.get(username=SIGNUP_INFO['username'])
+        response = self.client.post(SIGNUP_URL, self.SIGNUP_INFO)
+        user = User.objects.get(username=self.SIGNUP_INFO['username'])
 
         # Tests the auto login and redirect
         self.assertEqual(int(self.client.session['_auth_user_id']), user.pk, "User was not logged in")
@@ -28,16 +42,16 @@ class SignUpTestCase(TestCase):
 
     def test_existing_user(self):
         # Registers a new user
-        self.client.post(SIGNUP_URL, SIGNUP_INFO)
+        self.client.post(SIGNUP_URL, self.SIGNUP_INFO)
 
         # Tests registering with the same username
-        response = self.client.post(SIGNUP_URL, SIGNUP_INFO)
+        response = self.client.post(SIGNUP_URL, self.SIGNUP_INFO)
         self.assertContains(response, 'form-error')
 
     def test_signup_while_logged_in(self):
         # Tests accessing the signup page while already logged in
-        self.client.post(SIGNUP_URL, SIGNUP_INFO)
-        self.client.post(LOGIN_URL, LOGIN_INFO)
+        self.client.post(SIGNUP_URL, self.SIGNUP_INFO)
+        self.client.post(LOGIN_URL, self.LOGIN_INFO)
         response = self.client.get(SIGNUP_URL)
         self.assertRedirects(response, HOME_URL)
 
@@ -48,24 +62,33 @@ class LoginTestCase(TestCase):
     """
     def setUp(self):
         self.client = Client()
-        self.client.post(SIGNUP_URL, SIGNUP_INFO)
+        self.password = random_string()
+        self.user = UserFactory.create(password=self.password)
+        self.LOGIN_INFO = {
+            'username': self.user.username,
+            'password': self.password,
+        }
+        self.INCORRECT_LOGIN_INFO = {
+            'username': 'X' + self.user.username,
+            'password': 'X' + self.password,
+        }
+
+        self.user.save()
 
     def test_standard(self):
         # Tests a standard logging in of a user
-        response = self.client.post(LOGIN_URL, LOGIN_INFO)
-        user = User.objects.get(username=LOGIN_INFO['username'])
-        self.assertEqual(int(self.client.session['_auth_user_id']), user.pk,
+        response = self.client.post(LOGIN_URL, self.LOGIN_INFO)
+        self.assertEqual(int(self.client.session['_auth_user_id']), self.user.pk,
                          "User was not logged in")
         self.assertRedirects(response, HOME_URL)
 
     def test_wrong_user(self):
         # Tests a logging in of a user with incorrect information
-        response = self.client.post(LOGIN_URL, {'username': 'Name',
-                                                'password': 'wrongpassword'})
+        response = self.client.post(LOGIN_URL, self.INCORRECT_LOGIN_INFO)
         self.assertContains(response, 'form-error')
 
     def test_login_while_logged_in(self):
         # Tests accessing the signup page while already logged in
-        self.client.post(LOGIN_URL, LOGIN_INFO)
+        self.client.post(LOGIN_URL, self.LOGIN_INFO)
         response = self.client.get(LOGIN_URL)
         self.assertRedirects(response, HOME_URL)
