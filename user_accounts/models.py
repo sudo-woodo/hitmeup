049b8d29360b49b0ec67812700a6506f11cda320
Ethan Chan <metakirby5@gmail.com>
2015-05-01 22:58:22 -0700

    basic models

--- user_accounts/models.py
+++ user_accounts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    name = models.CharField(max_length=256)
+    out_friends = models.ManyToManyField('self', through='Friendship', symmetrical=False,
+                                              related_name='in_friends')
+
+    def __unicode__(self):
+        return self.name
+
+class Friendship(models.Model):
+    from_friend = models.ForeignKey(UserProfile, related_name='out_friendships')
+    to_friend = models.ForeignKey(UserProfile, related_name='in_friendships')
+    level = models.IntegerField(default=0)
+
+    class Meta:
+        unique_together = ('from_friend', 'to_friend')
+
+    def __unicode__(self):
+        return '%s --(%s)--> %s' % (self.from_friend, self.level, self.to_friend)
+
