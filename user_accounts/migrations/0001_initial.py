6a146854c884a4d188ce2edf5e966425398bacb0
Ethan Chan <metakirby5@gmail.com>
2015-05-02 07:37:05 -0700

    model clarity, validations, and basic methods

--- user_accounts/migrations/0001_initial.py
+++ user_accounts/migrations/0001_initial.py
@@ -2,11 +2,13 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('auth', '0006_require_contenttypes_0002'),
     ]
 
     operations = [
@@ -14,26 +16,25 @@ class Migration(migrations.Migration):
             name='Friendship',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('level', models.IntegerField(default=0)),
+                ('accepted', models.BooleanField(default=False)),
             ],
         ),
         migrations.CreateModel(
             name='UserProfile',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('name', models.CharField(max_length=256)),
-                ('out_friends', models.ManyToManyField(related_name='in_friends', through='user_accounts.Friendship', to='user_accounts.UserProfile')),
+                ('user', models.OneToOneField(related_name='profile', primary_key=True, serialize=False, to=settings.AUTH_USER_MODEL)),
+                ('outgoing_friends', models.ManyToManyField(related_name='incoming_friends', through='user_accounts.Friendship', to='user_accounts.UserProfile')),
             ],
         ),
         migrations.AddField(
             model_name='friendship',
             name='from_friend',
-            field=models.ForeignKey(related_name='out_friendships', to='user_accounts.UserProfile'),
+            field=models.ForeignKey(related_name='outgoing_friendships', to='user_accounts.UserProfile'),
         ),
         migrations.AddField(
             model_name='friendship',
             name='to_friend',
-            field=models.ForeignKey(related_name='in_friendships', to='user_accounts.UserProfile'),
+            field=models.ForeignKey(related_name='incoming_friendships', to='user_accounts.UserProfile'),
         ),
         migrations.AlterUniqueTogether(
             name='friendship',
