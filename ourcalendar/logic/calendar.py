e076829412b8e802c2bb44e1b4ccd1ca6bf38ff4
Jeffrey Chen <ultimatedbz@gmail.com>
2015-05-03 13:52:38 -0700

    changed code that actually used the models. mainly in logic/calendar.py

--- ourcalendar/logic/calendar.py
+++ ourcalendar/logic/calendar.py
@@ -1,13 +1,13 @@
 from ourcalendar.models import Calendar, Event
 
 
-def get_events(user):
+def get_events(owner):
     """Gets all events from a user's calendars."""
     calendars = {}
 
-    for c in Calendar.objects.filter(user=user):
+    for c in Calendar.objects.filter(owner=owner):
         events = Event.objects.filter(calendar=c).values('id', 'start', 'end', 'title',
-                                                         'location', 'description', 'users')
+                                                         'location', 'description')
         for e in events:
             e['start'] = e['start'].strftime('%Y-%m-%dT%H:%M:%S')
             e['end'] = e['end'].strftime('%Y-%m-%dT%H:%M:%S')
