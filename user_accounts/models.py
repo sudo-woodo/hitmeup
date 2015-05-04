ba535815af8d5aa7dc5cbf87f5a17e8931327fb7
Ethan Chan <metakirby5@gmail.com>
2015-05-04 03:49:29 -0700

    change bio to textfield

--- user_accounts/models.py
+++ user_accounts/models.py
@@ -15,7 +15,7 @@ class UserProfile(models.Model):
         message="Phone number must be entered in the format: "
                 "'+999999999'. Up to 15 digits allowed.")
     phone = models.CharField(max_length=16, validators=[phone_regex], blank=True)
-    bio = models.CharField(max_length=300, blank=True)
+    bio = models.TextField(max_length=300, blank=True)
 
     def __unicode__(self):
         return self.user.username
