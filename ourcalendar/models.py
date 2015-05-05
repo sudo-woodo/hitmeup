3a29782996c0cd1de9cfeedf98f7c4f0786af6d5
Ethan Chan <metakirby5@gmail.com>
2015-05-04 17:15:56 -0700

    add hash to hex color

--- ourcalendar/models.py
+++ ourcalendar/models.py
@@ -10,10 +10,10 @@ class Calendar(models.Model):
     owner = models.ForeignKey(UserProfile, related_name='calendars')
     title = models.CharField(max_length=200)
     color_regex = RegexValidator(
-        regex=r'^[\dA-F]{6}',
+        regex=r'^#[\dA-F]{6}',
         message="Color must be in 6-digit hex format."
     )
-    color = models.CharField(max_length=6, validators=[color_regex])
+    color = models.CharField(max_length=7, validators=[color_regex])
     privacy = models.IntegerField(default=0)
 
     def __unicode__(self):
@@ -23,7 +23,7 @@ class Calendar(models.Model):
 @receiver(post_save, sender=UserProfile)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        Calendar.objects.create(owner=instance, title='Default', color='267F00')
+        Calendar.objects.create(owner=instance, title='Default', color='#267F00')
 
 
 def hour_from_now():
