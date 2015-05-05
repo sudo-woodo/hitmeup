366f5a25176e0e2ecb961704d17b6eac3c790ea6
Richard Lin <rgl001@ucsd.edu>
2015-05-04 17:47:24 -0700

    Add authentication; certain requests only work if correct user is logged in

--- user_accounts/api.py
+++ user_accounts/api.py
@@ -1,5 +1,6 @@
 from restless.dj import DjangoResource
 from restless.preparers import FieldsPreparer
+from restless.exceptions import Unauthorized
 
 from .models import UserProfile
 
@@ -17,9 +18,8 @@ class UserProfileResource(DjangoResource):
         'phone': 'phone',
     })
 
-    # Fix me later?
     def is_authenticated(self):
-        return True
+        return self.request.user.is_authenticated()
 
     # GET /api/users/
     # Gets a list of all active users
@@ -35,6 +35,10 @@ class UserProfileResource(DjangoResource):
     # Updates a user's info with id=pk. Assumes specified user exists,
     # otherwise error is returned.  This is to prevent user creation.
     def update(self, pk):
+        if self.request.user.id != int(pk):
+            raise Unauthorized('Not authorized to update '
+                               'another user\'s profile.')
+
         profile = UserProfile.objects.get(user__id=pk)
 
         profile.user.email = self.data['email']
@@ -47,6 +51,9 @@ class UserProfileResource(DjangoResource):
     # DELETE /api/users/<pk>/
     # Sets a user to inactive with id=pk
     def delete(self, pk):
+        if self.request.user.id != int(pk):
+            raise Unauthorized('Not authorized to delete '
+                               'another user\'s profile.')
         profile = UserProfile.objects.get(user__id=pk)
         profile.user.is_active = False
         profile.user.save()
@@ -65,18 +72,25 @@ class FriendshipResource(DjangoResource):
         'phone': 'phone',
     })
 
-    # Fix me later?
     def is_authenticated(self):
-        return True
+        return self.request.user.is_authenticated()
 
     # GET /api/users/<user>/friends/
     # Gets a list of friends of a user with id=user
     def list(self, user):
+        if self.request.user.id != int(user):
+            raise Unauthorized('Not authorized to view '
+                               'another user\'s friends.')
+
         return UserProfile.objects.get(user__id=user).friends
 
     # PUT /api/users/<user>/friends/<pk>/
     # Adds a friendship of users: 'user' -> 'pk'
     def update(self, user, pk):
+        if self.request.user.id != int(user):
+            raise Unauthorized('Not authorized to add to '
+                               'another user\'s friends.')
+
         this = UserProfile.objects.get(user__id=user)
         other = UserProfile.objects.get(user__id=pk)
 
@@ -85,6 +99,10 @@ class FriendshipResource(DjangoResource):
     # DELETE /api/users/<user>/friends/<pk>/
     # Removes a friendship of users: 'user <-> 'pk'
     def delete(self, user, pk):
+        if self.request.user.id != int(user):
+            raise Unauthorized('Not authorized to remove '
+                               'another user\'s friends.')
+
         this = UserProfile.objects.get(user__id=user)
         other = UserProfile.objects.get(user__id=pk)
 
