0c68d3050e67eaa18fe2083aa9315bdfb47b7bda
Kavin <azncelebration@gmail.com>
2015-05-05 23:55:24 -0700

    Changed friend data for demo presentation

--- user_accounts/static/user_accounts/js/friends_list.jsx
+++ user_accounts/static/user_accounts/js/friends_list.jsx
@@ -1,79 +1,87 @@
 (function(React, $, _) {
+    'use strict';
+
     var cx = React.addons.classSet;
+
+    /*$(document).on('mouseenter', ".info", function () {
+     var $this = $(this);
+     if (this.width < this.scrollWidth) {
+         $this.tooltip({
+             title: $this.text(),
+             placement: "bottom"
+         });
+         $this.tooltip('show');
+     }
+    });*/
+
     var data = [
         {
-            'name': 'Ethan Ochinchin Chan',
-            'picture_url': 'http://i.ytimg.com/vi/90EAmumpAME/maxresdefault.jpg',
+            'name': 'Gary Gillespie',
+            'picture_url': 'https://media.licdn.com/mpr/mpr/shrink_200_200/p/2/000/0db/205/1492be8.jpg',
             'free': true,
-            'email': 'ochinchin@daisuke.edu',
+            'email': 'coconutwaterlvr@hotmail.com',
             'phone': '800-124-1457',
             'fav': true
         },
         {
-            'name': 'RichardtheGuy',
-            'picture_url': 'http://i.ytimg.com/vi/82yHd99YxnY/maxresdefault.jpg',
+            'name': 'RickyOrd',
+            'picture_url': 'http://cseweb.ucsd.edu/~ricko/images/Rick-Headshot.jpg',
             'free': false,
-            'email': 'euphoira@athiesm.gov',
+            'email': 'CAFEBABE@msn.com',
             'phone': '812-463-7334',
             'fav': true
         },
         {
-            'name': 'xXl33t_420_noscopXx',
+            'name': 'Massimiliano "MAX" Menarini',
             'picture_url':
-                'https://pbs.twimg.com/profile_images/1550541334/' +
-                'eye_reasonably_small_400x400.jpg',
+                'https://sosa.ucsd.edu/confluence/download/attachments/1212424/mmenarini_tn.jpg?version=1&modificationDate=1255626677000',
             'free': false,
-            'email': '360@getrekt.biz',
+            'email': 'agile@aim.net',
             'phone': '134-643-8754',
             'fav': false
         },
         {
-            'name': 'kevinChan',
+            'name': 'Papa Phil',
             'picture_url':
-                'http://i.telegraph.co.uk/multimedia/archive/02701/' +
-                'kim_2701423b.jpg',
+                'http://users.sdsc.edu/~phil/images/phil.jpg',
             'free': false,
-            'email': 'supremeleader@northkorea.northkorea',
+            'email': 'vimenthusiast@yahoo.com',
             'phone': '942-325-2678',
             'fav': true
         },
         {
-            'name': 'BreastBobFin',
+            'name': 'Barack Obama',
             'picture_url':
-                'http://images.gmanews.tv/v3/webpics/v3/2014/07/' +
-                '2014_07_01_12_49_56.jpg',
+                'http://api.ning.com/files/2R8JQXTOEkNOw74rkMxAns-rsBoRXm3osCtTiAzhQXMv5rjfjMHGEW6oTxHBdSrHBCb1Y2T20Yjmnx0ZjS*EYsk1msNWH-9f/ObamaFunnyFace.jpg',
             'free': true,
-            'email': 'orewa@yume.da',
+            'email': 'change@whitehouse.gov',
             'phone': '321-542-YUME',
             'fav': false
         },
         {
-            'name': 'Italiano',
+            'name': 'Snoop Dogg',
             'picture_url':
-                'http://img3.wikia.nocookie.net/__cb20100719141605/logopedia/' +
-                'images/9/98/Chef_Boyardee.png',
+                'http://assets.rollingstone.com/assets/images/story/snoop-dogg-talks-holograms-2pac-and-chronic-in-web-chat-20130509/snoop-624-1368121236.jpg',
             'free': false,
-            'email': 'agile@iteration.com',
+            'email': 'snoop@sdsu.edu',
             'phone': '568-234-3800',
             'fav': false
         },
         {
-            'name': 'naruto',
+            'name': 'Phreak',
             'picture_url':
-                'http://fc00.deviantart.net/fs71/i/2011/321/0/1/' +
-                'my_naruto_cosplay_by_maverickajo-d4ghfz8.jpg',
+                'http://i.ytimg.com/vi/RAIq5V6LqPM/maxresdefault.jpg',
             'free': true,
-            'email': 'believe@it.org',
+            'email': 'tonsofdamage@gmail.com',
             'phone': '123-855-8933',
             'fav': false
         },
         {
-            'name': 'Pool',
+            'name': 'Joseph Joestar',
             'picture_url':
-                'http://www.cee1.org/sites/www.cee1.org/files/' +
-                'images/SwimmingPool_0.jpg',
+                'http://www.learnspanishtoday.com/img/businessman.jpg',
             'free': false,
-            'email': 'swim@water.nz',
+            'email': 'hamon@gmail.com',
             'phone': '847-664-8436',
             'fav': true
         }
@@ -99,12 +107,77 @@
         }
     });
 
+    var FriendInfo = React.createClass({
+        render: function() {
+            var iClasses = {
+                'fa': true,
+                'fa-li': true
+            };
+            iClasses[this.props.icon] = true;
+
+            return (
+                <li>
+                    <i className={ cx(iClasses) }></i>
+                    { this.props.children }
+                </li>
+            )
+        }
+
+    });
+
     var FriendBox = React.createClass({
         render: function() {
-            var friendName = {
-                name: this.props.friend.name,
-                fav: this.props.friend.fav
-            };
+            var infoItems = [
+                {
+                    icon:
+                        this.props.friend.fav ? 'fa-heart' : 'fa-heart-o'
+                    ,
+                    info:
+                        <span className="info">
+                            <strong>{this.props.friend.name}</strong>
+                        </span>
+                },
+                {
+                    icon:
+                        this.props.friend.free ? 'fa-check-circle' : 'fa-clock-o'
+                    ,
+                    info:
+                        <span className={'info ' +
+                            (this.props.friend.free ? 'free' : 'busy')
+                        }>
+                            {this.props.friend.free ? 'Free' : 'Busy'}
+                        </span>
+                },
+                {
+                    icon: 'fa-envelope',
+                    info:
+                        <span className="info">
+                            {this.props.friend.email}
+                        </span>
+                },
+                {
+                    icon: 'fa-phone',
+                    info:
+                        <span className="info">
+                            {this.props.friend.phone}
+                        </span>
+                },
+                {
+                    icon: 'fa-calendar-o',
+                    info: <a href="" className="calendar-link info">
+                        View my Calendar
+                    </a>
+                }
+            ];
+
+            var infoNodes = infoItems.map(function(item) {
+                return (
+                    <FriendInfo icon={item.icon}>
+                        {item.info}
+                    </FriendInfo>
+                );
+            });
+
             return (
                 <div className="friend-box">
                     <div className="col-xs-12 col-sm-6 col-md-4">
@@ -112,36 +185,13 @@
                             <div className="friend-info panel-body">
                                 <div className="profile-pic-container">
                                     <FriendPic
-                                        friendPic=
+                                        pic=
                                             {this.props.friend.picture_url}
                                     />
                                 </div>
                                 <div className="profile-info-container">
                                     <ul className="fa-ul">
-                                        <FriendName
-                                            friendName={ friendName }
-                                        />
-
-                                        <FriendFree
-                                            friendFree=
-                                                {
-                                                    this.props.friend.free
-                                                }
-                                        />
-
-                                        <FriendEmail
-                                            friendEmail=
-                                                {
-                                                    this.props.friend.email
-                                                }
-                                        />
-
-                                        <FriendPhone
-                                            friendPhone=
-                                                {
-                                                    this.props.friend.phone
-                                                }
-                                        />
+                                        { infoNodes }
                                     </ul>
                                 </div>
                             </div>
@@ -156,94 +206,17 @@
         render: function() {
             return (
                 <img
-                    src={ this.props.friendPic }
-                    alt="test"
+                    src={ this.props.pic }
+                    alt="Picture was not found"
                     className="profile-pic img-circle"
                 />
             );
         }
     });
 
-    var FriendName = React.createClass({
-        render: function() {
-            return (
-                <li>
-                    <i
-                        className={cx({
-                            'fa': true,
-                            'fa-li': true,
-                            'fa-heart':
-                                this.props.friendName.fav,
-                            'fa-heart-o':
-                                !this.props.friendName.fav
-                        })}
-                    ></i>
-
-                    <strong>
-                        { this.props.friendName.name }
-                    </strong>
-                </li>
-            );
-        }
-    });
-
-    var FriendFree = React.createClass({
-        render: function() {
-            return (
-                <li>
-                    <i className={cx({
-                        'fa': true,
-                        'fa-li': true,
-                        'fa-check-circle':
-                            this.props.friendFree,
-                        'fa-clock-o':
-                            !this.props.friendFree
-                    })}></i>
-
-                    <span className={cx({
-                        'free':
-                            this.props.friendFree,
-                        'busy':
-                            !this.props.friendFree
-                    })}>
-                        {
-                            this.props.friendFree
-                                ? 'Free'
-                                : 'Busy'
-                        }
-                    </span>
-
-
-                </li>
-            );
-        }
-    });
-
-    var FriendEmail = React.createClass({
-        render: function() {
-            return (
-                <li>
-                    <i className="fa-li fa fa-envelope"></i>
-                    { this.props.friendEmail }
-                </li>
-            );
-        }
-    });
-
-    var FriendPhone = React.createClass({
-        render: function() {
-            return (
-                <li>
-                    <i className="fa-li fa fa-phone"></i>
-                    { this.props.friendPhone }
-                </li>
-            );
-        }
-    });
-
     React.render(
         <FriendsList data={data}/>,
-        document.getElementById('friends-list')
+        document.getElementById('friends-list-container')
     );
 })(window.React, window.jQuery, window._);
 
