4897406521afe0a344a6b149291427af0965bac1
Jeffrey Chen <ultimatedbz@gmail.com>
2015-05-06 00:59:16 -0700

    got GET to work

--- notifications/urls.py
+++ notifications/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import patterns, url, include
+from django.conf.urls import url, include
 
 from api import NotificationResource
 import views
 
-urlpatterns = patterns[
+urlpatterns = [
     url(r'^$', views.list, name='list'),
     url(r'^api/$', include(NotificationResource.urls())),
 ]
