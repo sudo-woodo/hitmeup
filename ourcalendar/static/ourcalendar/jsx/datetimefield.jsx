93385fb670b72d9ab655ed35b58680df954d423e
Richard Lin <rgl001@ucsd.edu>
2015-05-07 01:25:02 -0700

    Fix datepicker minDate/maxDate bug where incorrect min/max datetimes were persisting

--- ourcalendar/static/ourcalendar/jsx/datetimefield.jsx
+++ ourcalendar/static/ourcalendar/jsx/datetimefield.jsx
@@ -6,22 +6,35 @@ var DateTimeField = React.createClass({
     },
 
     componentDidMount: function() {
-        console.log('a');
-        $('#datetimepicker6').datetimepicker();
-        $('#datetimepicker7').datetimepicker();
-        $("#datetimepicker6").on("dp.change", function (e) {
-            $('#datetimepicker7').data("DateTimePicker").minDate(e.date);
+        $('#start-picker').datetimepicker();
+        $('#end-picker').datetimepicker();
+
+        // Linking start, end datetime pickers
+        $("#start-picker").on("dp.change", function (e) {
+            if (e.date)
+                $('#end-picker').data("DateTimePicker").minDate(e.date);
+            else
+                $('#end-picker').data("DateTimePicker").minDate(false);
         });
-        $("#datetimepicker7").on("dp.change", function (e) {
-            $('#datetimepicker6').data("DateTimePicker").maxDate(e.date);
+        $("#end-picker").on("dp.change", function (e) {
+            if (e.date)
+                $('#start-picker').data("DateTimePicker").maxDate(e.date);
+            else
+                $('#start-picker').data("DateTimePicker").maxDate(false);
         });
+
+        // Reset min, max dates when event creation modal is dismissed
+        $('#create-event-modal').on('hidden.bs.modal', function (e) {
+            $('#start-picker').data("DateTimePicker").maxDate(false);
+            $('#end-picker').data("DateTimePicker").minDate(false);
+        })
     },
 
     render: function() {
        return (
             <div>
                 <div className="form-group">
-                    <div className='input-group date' id='datetimepicker6'>
+                    <div className='input-group date' id='start-picker'>
                         <input type='text' ref="start" className="form-control" placeholder="Start time" />
                             <span className="input-group-addon">
                             <span className="glyphicon glyphicon-calendar"></span>
@@ -29,7 +42,7 @@ var DateTimeField = React.createClass({
                     </div>
                 </div>
                 <div className="form-group">
-                    <div className='input-group date' id='datetimepicker7'>
+                    <div className='input-group date' id='end-picker'>
                         <input type='text' ref="end" className="form-control" placeholder="End time" />
                         <span className="input-group-addon">
                             <span className="glyphicon glyphicon-calendar"></span>
