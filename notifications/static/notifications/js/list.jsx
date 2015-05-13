5ab3b2449ba65ea672ed669277ec71f46a036da5
Ethan Chan <metakirby5@gmail.com>
2015-05-12 18:33:47 -0700

    single-column notifications

--- notifications/static/notifications/js/list.jsx
+++ notifications/static/notifications/js/list.jsx
@@ -6,43 +6,34 @@
     var Notification = React.createClass({
         render: function() {
             return (
-                <div className="col-xs-12 col-md-6">
-                    <a className={cx({
-                        'panel': true,
-                        'panel-default': true,
-                        'notification': true,
-                        'read': this.props.data.read
-                    })} href={this.props.data.action}>
-                        <div className="panel-body">
-                            <div className="notification-main">
-                                <img className="notification-img img-thumbnail"
-                                     src={this.props.data.image} />
-                                <div className="notification-text">
-                                    {this.props.data.text}
-                                </div>
-                                <div className="clearfix"></div>
+                <a className={cx({
+                    'panel': true,
+                    'panel-default': true,
+                    'notification': true,
+                    'read': this.props.data.read
+                })} href={this.props.data.action}>
+                    <div className="panel-body">
+                        <div className="notification-main">
+                            <img className="notification-img img-thumbnail"
+                                 src={this.props.data.image} />
+                            <div className="notification-text">
+                                {this.props.data.text}
                             </div>
-                            <div className="notification-time">
-                                {this.props.data.time}
-                            </div>
+                            <div className="clearfix"></div>
                         </div>
-                    </a>
-                </div>
+                        <div className="notification-time">
+                            {this.props.data.time}
+                        </div>
+                    </div>
+                </a>
             );
         }
     });
 
     var NotificationList = React.createClass({
         render: function() {
-            var notifications = _.chunk(this.props.notifications, 2)
-                .map(function(notifs) {
-                    return (
-                        <div className="row">
-                            {notifs.map(function(notif) {
-                                return <Notification data={notif} />
-                            })}
-                        </div>
-                    );
+            var notifications = this.props.notifications.map(function(notif) {
+                return <Notification data={notif} />
             });
 
             return (
