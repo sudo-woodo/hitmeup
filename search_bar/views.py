8545e7772cda30e863e4e9bb1b172df3487b95d7
Bob Fan <bobfan03@gmail.com>
2015-05-20 03:02:26 -0700

    made test pages for auto complete

--- search_bar/views.py
+++ search_bar/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.views.generic import View
+from django.core.urlresolvers import reverse
 
 from .forms import UserSearchForm
 import simplejson as json
@@ -8,38 +9,27 @@ from haystack.query import SearchQuerySet
 
 class Search(View):
     def get(self, request):
-        # Second parameter is the default value.
-        sqs = SearchQuerySet().autocomplete(user_auto=request.GET.get('q', "Error: No match"))[:5]
-        print sqs
-        suggestions = [result.title for result in sqs]
-        print "ey"
-        # Make sure you return a JSON object, not a bare list.
-        # Otherwise, you could be vulnerable to an XSS attack.
-        the_data = json.dumps({
-            'results': suggestions
-        })
-        return HttpResponse(the_data, content_type='application/json')
-        '''
         form = UserSearchForm(data=request.GET)
         user = form.search() # form.search() returns a SearchQuerySet
+        if user:
+            # Best_match() will get the SearchResult, then you get the user and the username
+            username = user.best_match().object.get_username()
+            # return HttpResponseRedirect(reverse('user_accounts:user_profile',username=username))
+            return render(request, 'search_bar/usernames.jinja', {'username': username}) # eventually change this to render user profile
 
-        # best_match() will get the SearchResult, then you get the user and the username
-        username = user.best_match().object.get_username()
-        return HttpResponseRedirect(reverse('user_accounts:user_profile',username=username))
-        return render(request, 'search_bar/usernames.jinja', {'username': username})''' # eventually change this to render user profile
+        # Not an exact username, need auto-complete!
+        else:
+            # The second parameter is the default value. Returns SearchResult object.
+            sqs = SearchQuerySet().autocomplete(user_auto=request.GET.get('q', "Error: No match"))[:5]
+            suggestions = [result.object.get_username() for result in sqs]
 
-    def auto_complete(self, request):
-        sqs = SearchQuerySet().autocomplete(content_auto=request.GET.get('q', ''))[:5]
-        suggestions = [result.title for result in sqs]
+            # Make sure you return a JSON object, not a bare list,
+            # otherwise you could be vulnerable to an XSS attack.
+            the_data = json.dumps({
+                'results': suggestions
+            })
+            return HttpResponse(the_data, content_type='application/json') # change this later
 
-        print("ey")
-        # Make sure you return a JSON object, not a bare list.
-        # Otherwise, you could be vulnerable to an XSS attack.
-        the_data = json.dumps({
-            'results': suggestions
-        })
-        return HttpResponse(the_data, content_type='application/json')
-
-# probably won't need this in future since search is in navbar
+# Probably won't need this in future since search is in navbar
 def SearchBase(request):
     return render(request, 'search_bar/search.jinja')
