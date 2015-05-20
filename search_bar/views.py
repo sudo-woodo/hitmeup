6faf8190956381a3693cd22cef9c16379594d6dc
Bob Fan <bobfan03@gmail.com>
2015-05-20 02:10:20 -0700

    got working auto complete

--- search_bar/views.py
+++ search_bar/views.py
@@ -8,18 +8,31 @@ from haystack.query import SearchQuerySet
 
 class Search(View):
     def get(self, request):
+        # Second parameter is the default value.
+        sqs = SearchQuerySet().autocomplete(user_auto=request.GET.get('q', "Error: No match"))[:5]
+        print sqs
+        suggestions = [result.title for result in sqs]
+        print "ey"
+        # Make sure you return a JSON object, not a bare list.
+        # Otherwise, you could be vulnerable to an XSS attack.
+        the_data = json.dumps({
+            'results': suggestions
+        })
+        return HttpResponse(the_data, content_type='application/json')
+        '''
         form = UserSearchForm(data=request.GET)
         user = form.search() # form.search() returns a SearchQuerySet
 
         # best_match() will get the SearchResult, then you get the user and the username
         username = user.best_match().object.get_username()
-        '''return HttpResponseRedirect(reverse('user_accounts:user_profile',username=username))'''
-        return render(request, 'search_bar/usernames.jinja', {'username': username}) # eventually change this to render user profile
+        return HttpResponseRedirect(reverse('user_accounts:user_profile',username=username))
+        return render(request, 'search_bar/usernames.jinja', {'username': username})''' # eventually change this to render user profile
 
-    def autocomplete(request):
+    def auto_complete(self, request):
         sqs = SearchQuerySet().autocomplete(content_auto=request.GET.get('q', ''))[:5]
         suggestions = [result.title for result in sqs]
 
+        print("ey")
         # Make sure you return a JSON object, not a bare list.
         # Otherwise, you could be vulnerable to an XSS attack.
         the_data = json.dumps({
