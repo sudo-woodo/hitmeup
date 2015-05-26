d79da7cc679a261268a318e87d1ea31281a73ee0
Ethan Chan <metakirby5@gmail.com>
2015-05-25 23:04:35 -0700

    require auth for search, prep for searchbox

--- search_bar/urls.py
+++ search_bar/urls.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    url(r'^results/$', views.Search.as_view(), name='search'),
+    url(r'^results/$', views.UserSearch.as_view(), name='search'),
 ]
--- search_bar/views.py
+++ search_bar/views.py
@@ -1,31 +1,56 @@
 from django.core.urlresolvers import reverse
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
+from django.http.response import JsonResponse
 from django.views.generic import View
 from haystack.query import SearchQuerySet
 import json
 from .forms import UserSearchForm
 
-class Search(View):
-    def get(self, request):
-        form = UserSearchForm(data=request.GET)
+
+NUM_AUTOCOMPLETE = 5
+
+
+class UserSearch(View):
+
+    @staticmethod
+    def do_search(data):
+        """
+        User search logic. Returns a tuple of (username, [suggestions]).
+        username can be blank, and suggestions can be empty on error.
+
+        :param data: Form data
+        :return: (string, [string])
+        """
+        form = UserSearchForm(data=data)
         user = form.search() # form.search() returns a SearchQuerySet
-        if user:
-            # Best_match() will get the SearchResult, then you get the user and the username
-            username = user.best_match().object.get_username()
+
+        # Best_match() will get the SearchResult, then you get the user and the username
+        username = user.best_match().object.get_username() if user else None
+
+        # The second parameter is the default value. Returns SearchResult object.
+        try:
+            sqs = SearchQuerySet().autocomplete(
+                user_auto=data['q'])[:NUM_AUTOCOMPLETE]
+            suggestions = [result.object.get_username() for result in sqs]
+        except KeyError:
+            suggestions = []
+
+        return (username, suggestions)
+
+    # For page view
+    def get(self, request):
+        username, suggestions = self.do_search(request.GET)
+
+        if username:
             return HttpResponseRedirect(
                 reverse('user_accounts:user_profile', args=(username,))
             )
-
-        # Not an exact username, need auto-complete!
         else:
-            # The second parameter is the default value. Returns SearchResult object.
-            sqs = SearchQuerySet().autocomplete(user_auto=request.GET.get('q', "Error: Key 'q' not found."))[:5]
-            suggestions = [result.object.get_username() for result in sqs]
+            # TODO: make a nice search results page
+            return JsonResponse({'suggestions': suggestions})
 
-            # Make sure you return a JSON object, not a bare list,
-            # otherwise you could be vulnerable to an XSS attack.
-            # TODO: return a list of user profiles once its branch is merged to master
-            the_data = json.dumps({
-                'results': suggestions
-            })
-            return HttpResponse(the_data, content_type='application/json') # change this later
+    # For autocomplete box
+    def post(self, request):
+        username, suggestions = self.do_search(request.POST)
+
+        return JsonResponse({'suggestions': suggestions})
