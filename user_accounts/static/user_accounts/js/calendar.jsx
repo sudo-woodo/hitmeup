2dca406ea2fbf1cab5c939033f6c254b6a0cb360
Richard Lin <rgl001@ucsd.edu>
2015-05-27 16:44:20 -0700

    Add checkboxes to toggle visiblity of individual event sources

--- user_accounts/static/user_accounts/js/calendar.jsx
+++ user_accounts/static/user_accounts/js/calendar.jsx
@@ -4,7 +4,7 @@ var calendarReactor = (function($HMU, React, $, _)  {
     var BUSY_ID = "FRIEND_BUSY";
     var FREE_ID = "FRIEND_FREE";
 
-    var events = $HMU.user_events;
+    var user_events = $HMU.user_events;
     var friend_events_busy = $HMU.friend_events;
     var friend_events_free = $.extend(true, [], friend_events_busy);
     var should_display = $HMU.should_display;
@@ -34,9 +34,6 @@ var calendarReactor = (function($HMU, React, $, _)  {
         return event;
     });
 
-    $.merge(events, friend_events_busy);
-    $.merge(events, friend_events_free);
-
     var NotFriend = React.createClass({
         render: function()  {
             var icon = this.props.initial ? "fa fa-refresh fa-lg" : "fa fa-lock fa-lg";
@@ -65,7 +62,11 @@ var calendarReactor = (function($HMU, React, $, _)  {
                 var cal = this.refs.cal.getDOMNode();
 
                 $(cal).fullCalendar({
-                    events: events,
+                    eventSources: [
+                        user_events,
+                        friend_events_free,
+                        friend_events_busy
+                    ],
                     defaultView: "agendaWeek",
                     allDaySlot: false,
                     scrollTime: "08:00:00",
@@ -77,8 +78,14 @@ var calendarReactor = (function($HMU, React, $, _)  {
                         right: 'month,agendaWeek,agendaDay'
                     },
                     select: function(start, end) {
-                        $('#start-picker').data("DateTimePicker").date(start);
-                        $('#end-picker').data("DateTimePicker").date(end);
+                        var startPicker = $('#start-picker');
+                        var endPicker = $('#end-picker');
+
+                        // jQuery element's length is > 0 if it exists
+                        if (startPicker.length && endPicker.length) {
+                            startPicker.data("DateTimePicker").date(start);
+                            endPicker.data("DateTimePicker").date(end);
+                        }
                     }
                 });
             }
@@ -87,10 +94,62 @@ var calendarReactor = (function($HMU, React, $, _)  {
         render: function() {
             var window = this.state.initial ? <NotFriend initial={true} /> :
                 this.state.display ? <div ref='cal' id="calendar"></div> : <NotFriend initial={false} />;
-
+            var checkboxes = !this.state.initial && this.state.display && !$HMU.is_user ? <ShowEventOptions /> : "";
             return (
                 <div>
+                    {checkboxes}
                     {window}
+                </div>
+            );
+        }
+    });
+
+    var ShowEventOptions = React.createClass({
+        getInitialState: function() {
+            return  {
+                showSelf: true,
+                showFree: true,
+                showBusy: true
+            };
+        },
+
+        // TODO figure out how to get calendar DOM node react-ly
+        onChangeSelf: function() {
+            $('#calendar').fullCalendar(
+                this.state.showSelf ? 'removeEventSource' : 'addEventSource', user_events
+            );
+            this.setState({showSelf: !this.state.showSelf});
+        },
+
+        onChangeFree: function() {
+            $('#calendar').fullCalendar(
+                this.state.showFree ? 'removeEventSource' : 'addEventSource', friend_events_free
+            );
+            this.setState({showFree: !this.state.showFree});
+        },
+
+        onChangeBusy: function() {
+            $('#calendar').fullCalendar(
+                this.state.showBusy ? 'removeEventSource' : 'addEventSource', friend_events_busy
+            );
+            this.setState({showBusy: !this.state.showBusy});
+        },
+
+        render: function() {
+            return (
+                <div id="checkboxes">
+                    <label className="checkbox-inline">
+                        <input type="checkbox" id="showSelf" checked={this.state.showSelf} onChange={this.onChangeSelf} />
+                        Show my events
+                    </label>
+                    <label className="checkbox-inline">
+                        <input type="checkbox" id="showFree" checked={this.state.showFree} onChange={this.onChangeFree} />
+                        Show my friend's free times
+                    </label>
+                    <label className="checkbox-inline">
+                        <input type="checkbox" id="showBusy" checked={this.state.showBusy} onChange={this.onChangeBusy} />
+                        Show my friend's busy times
+                    </label>
                 </div>
             );
         }
