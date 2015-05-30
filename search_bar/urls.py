672c3288f44eab876cb05796da9f5d1cff27b2f7
Royston L Lai <RoystonL@s00-0000-10360-res.resnet.ucsd.edu>
2015-05-30 16:16:25 -0700

    Quite bare result page, build will fail

--- search_bar/urls.py
+++ search_bar/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
 from django.contrib import admin
 from . import views
+from django.contrib.auth.models import *
+from user_accounts.models import *
 
 urlpatterns = [
     url(r'^results/$', views.user_search, name='user_search'),
     url(r'^autocomplete/$', views.user_autocomplete, name='user_autocomplete'),
+    # url(r'^user/(?P<username>[\w.@+-]+)/$', views.UserProfile.as_view(),
+    #     name='user_profile'),
 ]
