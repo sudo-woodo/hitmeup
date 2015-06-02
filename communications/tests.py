c313b697692728e3b6797c66cc3d861016ffad25
Aaron Graham <aarong4743@optonline.net>
2015-06-02 01:07:16 -0700

    Notifcation Email Tests Added

--- communications/tests.py
+++ communications/tests.py
@@ -3,7 +3,11 @@ from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.test import TestCase, Client
 from django.utils.crypto import get_random_string
-from util.factories import UserFactory
+from util.factories import *
+from communications.emails import render_email
+from user_accounts.models import *
+from user_accounts.models import *
+from notifications.models import *
 
 SIGNUP_URL = reverse('user_accounts:signup')
 
@@ -37,3 +41,19 @@ class EmailTestCase(TestCase):
 
         # Checks subject to be welcome subject, to test that welcome email was sent
         self.assertEqual(mail.outbox[0].subject, 'Welcome to HitMeUp!')
+
+    def test_notification(self):
+        # testing notification emails for sending friend requests
+        # p, q, r = UserProfile.objects.all()[4:6]
+        p, q, r = [UserProfileFactory(user__email='sudowoodohitmeup@gmail.com') for _ in range(3)]
+        p.add_friend(q)
+        q.add_friend(r)
+        r.add_friend(p)
+        self.assertEqual(len(mail.outbox), 3)
+        self.assertEqual(mail.outbox[0].subject, 'A Notification from HitMeUp')
+        self.assertEqual(mail.outbox[0].subject, 'A Notification from HitMeUp')
+        self.assertEqual(mail.outbox[0].subject, 'A Notification from HitMeUp')
+
+
+
+
